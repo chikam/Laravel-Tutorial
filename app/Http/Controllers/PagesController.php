bc9b004ba25a01cf116b9ae25c77487768dbbc9e
chikam <c.coco.102@gmail.com>
2016-04-22 01:04:49 +0900

    Pass the values

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -8,7 +8,15 @@ use Illuminate\Http\Request;
 class PagesController extends Controller {
 
   public function about() {
-    return view('pages.about');
+    // $data = [];
+    // $data['first_name'] = "Hanako";
+    // $data['last_name'] = "Yamada";
+    // return view('pages.about', $data);
+
+    $first_name = "Hanako";
+    $last_name = "Yamada";
+    return view('pages.about', compact('first_name', 'last_name'));
+
   }
 
   public function contact() {
