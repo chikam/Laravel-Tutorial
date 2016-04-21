7f667cbe0abdb72d564168a97d1ba66ffe569999
chikam <c.coco.102@gmail.com>
2016-04-21 23:27:11 +0900

    Routing, Controller, View

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,16 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class TestController extends Controller {
+
+  public function test()
+  {
+    return view('test');
+  }
+
+
+}
