b31b78b575ee7263db04ebbec83c8ba1b247732c
chikam <c.coco.102@gmail.com>
2016-04-25 09:04:33 +0900

    mutator

--- app/Article.php
+++ app/Article.php
@@ -5,5 +5,18 @@ use Illuminate\Database\Eloquent\Model;
 class Article extends Model {
 
   protected $fillable = ['title', 'body', 'published_at'];
+  // 日付ミューテターを使う
+  protected $dates = ['published_at'];
 
+  public function getTitleAttribute($value)
+  {
+    //大文字に変換
+    return mb_strtoupper($value); 
+  }
+
+  public function setTitleAttribute($value)
+  {
+    //小文字に変換
+    $this->attributes['title'] = mb_strtolower($value);
+  }
 }
