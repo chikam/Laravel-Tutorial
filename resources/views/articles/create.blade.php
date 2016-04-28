b9d9b414da0513ad263193b24e547d617de7c13c
chikam <c.coco.102@gmail.com>
2016-04-28 22:36:20 +0900

    validation

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -1,10 +1,22 @@
 @extends('layout')
- 
+
 @section('content')
     <h1>Write a New Article</h1>
- 
+
     <hr/>
- 
+
+    {{-- エラー表示を追加 --}}
+    @if ($errors->any())
+      <div class="alert alert-danger">
+        <ul>
+          @foreach ($errors->all() as $error)
+            <li>{{ $error }}</li>
+          @endforeach
+        </ul>
+      </div>
+    @endif
+
+
     {!! Form::open(['url' => 'articles']) !!}
         <div class="form-group">
             {!! Form::label('title', 'Title:') !!}
@@ -17,9 +29,10 @@
         <div class="form-group">
             {!! Form::label('published_at', 'Publish On:') !!}
             {!! Form::input('date', 'published_at', date('Y-m-d'), ['class' => 'form-control']) !!}
-        </div>    
+        </div>
         <div class="form-group">
             {!! Form::submit('Add Article', ['class' => 'btn btn-primary form-control']) !!}
         </div>
     {!! Form::close() !!}
+  @stop
 @endsection
