e5e8269021f9671141f5e683cbda6044988a747e
chikam <c.coco.102@gmail.com>
2016-04-29 09:59:18 +0900

    add editButton

--- resources/views/articles/show.blade.php
+++ resources/views/articles/show.blade.php
@@ -1,11 +1,16 @@
 @extends('layout')
- 
+
 @section('content')
     <h1>{{ $article->title }}</h1>
- 
+
     <hr/>
- 
+
     <article>
         <div class="body">{{ $article->body }}</div>
     </article>
+
+    <br/>
+
+    {!! link_to(action('ArticlesController@edit', [$article->id]), '編集', ['class' => 'btn btn-primary']) !!}
+  @stop
 @endsection
