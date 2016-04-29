df1283019e39b198e6790a3674448464fe07f279
chikam <c.coco.102@gmail.com>
2016-04-29 09:18:58 +0900

    add scope

--- app/Article.php
+++ app/Article.php
@@ -1,5 +1,6 @@
 <?php namespace App;
 
+use Carbon\Carbon;
 use Illuminate\Database\Eloquent\Model;
 
 class Article extends Model {
@@ -11,12 +12,16 @@ class Article extends Model {
   public function getTitleAttribute($value)
   {
     //大文字に変換
-    return mb_strtoupper($value); 
+    return mb_strtoupper($value);
   }
 
   public function setTitleAttribute($value)
   {
     //小文字に変換
     $this->attributes['title'] = mb_strtolower($value);
+  }
+
+  public function scopePublished($query) {
+    $query->where('published_at', '<=', Carbon::now());
   }
 }
